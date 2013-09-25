a9ac16a542a381aa9f403fd9280daf34c514ba79
Jeremy Baker <jhubert@gmail.com>
2013-09-24 22:24:57 -0700

    Remove middleware because it's not supported by mixpanel-ruby

--- lib/mixpanel/engine.rb
+++ lib/mixpanel/engine.rb
@@ -3,11 +3,9 @@ require 'mixpanel-ruby'
 module Mixpanel
   class Engine < ::Rails::Railtie
 
-    Config = Struct.new(:token, :options, :middleware)
-    MiddlewareConfig = Struct.new(:use, :insert_js_last, :persist, :config)
+    Config = Struct.new(:token, :options)
 
-    config.mixpanel = Config.new(nil, {}, MiddlewareConfig.new(false, false, false, {}))
-
+    config.mixpanel = Config.new(nil, {})
 
     initializer 'mixpanel.setup' do |app|
       class << app
@@ -20,19 +18,7 @@ unable to interact with mixpanel.com. You can set your token with either the env
 variable `MIXPANEL_TOKEN` (recommended) or by setting `config.mixpanel.token` in your
 environment file directly.
 WARNING
-      app.mixpanel = Mixpanel::Tracker.new config.mixpanel.token, config.mixpanel.options
-    end
-
-    initializer 'mixpanel.middleware' do |app|
-      mixpanel = config.mixpanel
-      middleware = mixpanel.middleware
-      if middleware.use
-        config.middleware.use 'Mixpanel::Middleware', mixpanel.token, {
-          :insert_js_last => middleware.insert_js_last,
-          :persist => middleware.persist,
-          :config => middleware.config
-        }
-      end
+      app.mixpanel = Mixpanel::Tracker.new config.mixpanel.token
     end
   end
 end
